768edb22cb7d9f57dc4c15d1b68067a0fd32d480
Regis <reagis@gmail.com>
2018-09-04 16:57:43 -0300

    Alpha 1

--- forca.js
+++ forca.js
@@ -5,26 +5,23 @@ var io = require('socket-io')(http);
 const MAX_USERS = 10;
 //nome, pronto e pontuação
 var users = [];
-var possibleLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'];
 var usedLetters = [];
 var word = [];
 var tip;
 var idAsker;
 var joiningAllowed = true;
+var tries = 5;
 
 io.on('connection', manageClient);
 
 function manageClient(client) {
     client.on('join', clientJoin);
     client.on('toggleReady', clientReady);
-    client.on('chosenWord', clientChosenWord);
+    client.on('chooseWord', clientChooseWord);
     client.on('pickLetter', clientPickLetter);
     client.on('guess', clientGuess);
     client.on('start', startRound);
-
 }
-
-//setar o asker em algum momento
 
 function clientJoin(nome) {
     if (joiningAllowed) {
@@ -39,9 +36,21 @@ function clientJoin(nome) {
 
 function clientReady() {
     users[client.id].ready = !users[client.id].ready;
+    for (var each of users) {
+        if (!each.ready) {
+            io.emit('notAllPlayersReady');
+            return;
+        }
+    }
+    io.emit('showStartButton');
 }
 
-function clientChosenWord(chosenWord, tip) {
+function setAsker() {
+    var idAsker = Math.round(1 + Math.random() * users.length);
+    io.emit('askedChosen', users[idAsker]);
+}
+
+function clientChooseWord(chosenWord, tip) {
     if (client.id === idAsker) {
         for (var letter of chosenWord) {
             word.push({ letter: letter, guessed: false });
@@ -55,15 +64,28 @@ function clientChosenWord(chosenWord, tip) {
 
 function clientPickLetter(letter) {
     if (!usedLetters.includes(letter)) {
+        var guessed = false;
         for (var currentletter of word) {
             if (currentLetter.letter === letter) {
                 currentletter.guessed = true;
+                gussed = true;
                 break;
             }
         }
-        client.emit('correcLetterUser');
-        client.broadcast.emit('correctLetter', client.name);
+        if (guessed) {
+            client.emit('correcLetterUser');
+            client.broadcast.emit('correctLetter', client.name);
+        } else {
+            tries -= 1;
+            if (tries > 0) {
+                client.emit('wrongLetter', tries);
+            } else {
+                client.emit('endGame');
+                resetRoundInfo();
+            }
+        }
     }
+    usedLetters.push(letter);
 }
 
 function clientGuess(guess) {
@@ -79,16 +101,23 @@ function clientGuess(guess) {
     } else {
         client.emit('clientLostUser');
         client.broadcast.emit('clientLost', client.name);
-        //deleter usuario
-        //impedir de outros usuarios entrarem denovo até o fim,mas permitir assistir
+        users[client.id].ready = false;
     }
 }
 
-function startRound() {
+function resetRoundInfo() {
     usedLetters = [];
     word = [];
     tip = '';
     idAsker = null;
-
+    joiningAllowed = true;
+    tries = 5;
+    io.emit('newRoundReady');
 }
 
+function startRound() {
+    joiningAllowed = false;
+    io.emit('gameStart');
+}
+
+http.listen(3000);
